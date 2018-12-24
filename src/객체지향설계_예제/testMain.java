41f20e241f6e6ba44ddd31da2a036a9cd4a8898e
Hyeon Woo Jeong <qlgks1@naver.com>
2018-12-24 14:07:24 +0900

    init git

---
+++ src/객체지향설계_예제/testMain.java
@@ -0,0 +1,24 @@
+package 객체지향설계_예제;
+
+import 객체지향설계_예제.BaseGUI.BaseGUI;
+import 객체지향설계_예제.file.FileInOut;
+import 객체지향설계_예제.file.MemoJang;
+
+public class testMain {
+	
+	public static void main(String args[]) {
+		
+		new BaseGUI();
+		
+//		// for FileInOut class
+//		String Path1, Path2;
+//		Path1 = "test1.txt";
+//		Path2 = "test2.txt";
+//		new FileInOut(Path1, Path2);
+		
+//		// for MemoJang class
+//		new MemoJang();
+		
+	} // main
+	
+}
