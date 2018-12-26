a3c7c91dd0b5b0410a2fe386cb6947d335d4d719
Nuung <qlgks1@naver.com>
2018-12-26 16:15:04 +0900

    configue DB setting (in school computer)

--- src/객체지향설계_예제/testMain.java
+++ src/객체지향설계_예제/testMain.java
@@ -1,6 +1,10 @@
 package 객체지향설계_예제;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import 객체지향설계_예제.BaseGUI.BaseGUI;
+import 객체지향설계_예제.DB.DBconnection;
 import 객체지향설계_예제.Lambda.LambdaFormula;
 import 객체지향설계_예제.file.FileInOut;
 import 객체지향설계_예제.file.MemoJang;
@@ -11,6 +15,21 @@ public class testMain {
 		
 		// for swing GUI
 		new BaseGUI();
+		DBconnection dbcon = new DBconnection();
+		ResultSet resultAll = dbcon.getRecodeAll();
+		
+		try {
+			do { // next하면 포인트가 다음을 다르킨다 생각하자
+				String result_id = resultAll.getString("id");
+				String result_username = resultAll.getString("username");
+				String result_dept = resultAll.getString("dept");
+				String result_birth = resultAll.getString("birth");
+				String result_email = resultAll.getString("email");
+				System.out.println(result_id+", "+result_username+", "+result_dept+", "+result_birth+", "+result_email);
+			} while(resultAll.next()); // do - while : 첫번째 결과 row값도 실행하기 위해
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} // try - catch
 		
 //		// for FileInOut class
 //		String Path1, Path2;
